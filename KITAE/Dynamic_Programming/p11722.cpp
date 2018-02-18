4cd071f536b758e7b0453c3b5724b17d8648ab7b
SkynI25 <rlxo1651@daum.net>
2018-02-18 21:28:18 +0900

    Create p11722.cpp
    수열 A가 주어졌을 때, 가장 긴 감소하는 부분 수열을 구하는 프로그램을 작성하시오.
    [URI] https://www.acmicpc.net/problem/11722
    [소감] 가장 진 증가하는 수열을 찾는 문제와 유사하여 쉽게 풀 수 있었다.
    dp의 for문내에서 if (num[n] > num[i]) 에서 if (num[n] < num[i]) 로 수정해주어 오른쪽 끝에서 시작하여 왼쪽 끝으로 가면서 최대값을 재귀하면서 찾도록 하였다.

---
+++ KITAE/Dynamic_Programming/p11722.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+long long d[100001];
+long long num[100001];
+
+long long dp(int n) {
+
+	if (d[n] > 0)
+		return d[n];
+
+	// for (int i = n-1; i >= 0; i--) { 으로 해도 된다.
+	for (int i = n - 1; i >= 0; i--) {
+		if (num[n] < num[i])
+			d[n] = max(d[n], dp(i) + 1);
+	}
+
+	return d[n];
+}
+
+int main() {
+
+	int n = 0;
+
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		cin >> num[i];
+	}
+
+	long long ans = 0;
+	for (int i = n; i >= 1; i--) {
+		ans = max(ans, dp(i)+1);
+		// ans = max(ans, dp(i)+1); 으로 해도 된다.
+	}
+
+	cout << ans << endl;
+
+	return 0;
+}
